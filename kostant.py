339e42a9df68ece0876a700b16a523ed8eb474e2
dobryfranek <dobrowolski.franciszek@gmail.com>
2025-04-01 21:01:32 +0200

    aktualizacja

--- kostant.py
+++ kostant.py
@@ -1,4 +1,3 @@
-#test
 import threading
 import ctypes
 import random
@@ -17,7 +16,7 @@ pygame.init()
 
 WIDTH, HEIGHT = pygame.display.Info().current_w, pygame.display.Info().current_h
 FONT = pygame.font.SysFont("Consolas", int(WIDTH // 32))
-scr = pygame.display.set_mode((WIDTH, HEIGHT), vsync=True)
+scr = pygame.display.set_mode((WIDTH, HEIGHT))
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
@@ -28,7 +27,7 @@ PLAYER_HEIGHT = PLAYER_WIDTH * 0.9
 MATEUSZ = pygame.image.load(path("player.png"))
 MATEUSZ = pygame.transform.scale(MATEUSZ, (PLAYER_WIDTH, PLAYER_HEIGHT))
 
-ENEMY_WIDTH = WIDTH // 15
+ENEMY_WIDTH = WIDTH // 30
 ENEMY_HEIGHT = ENEMY_WIDTH * 0.9
 ENEMY = pygame.image.load(path("enemy.png")).convert_alpha()
 
@@ -53,12 +52,14 @@ class Player():
 
 class Enemy():
     def __init__(self, img: pygame.Surface):
-        self.img = pygame.transform.scale(img, (ENEMY_WIDTH, ENEMY_HEIGHT))
+        this_size = (ENEMY_WIDTH * random.uniform(0.9, 2), ENEMY_HEIGHT * random.uniform(0.9, 2))
+        self.img = pygame.transform.scale(img, this_size)
         self.rect = self.img.get_rect()
         self.rect.x = random.randint(0, WIDTH - self.rect.width)
         self.rect.y = -self.rect.height
         self.accel = 9.81 / 32
-        self.accel += random.uniform(-1/16, 1/8)
+        self.accel *= ((this_size[0] * this_size[1]) / 8192) ** 0.5
+        # self.accel += random.uniform(-1/16, 1/8)
         self.velocity_y = 1
     
     def update(self):
@@ -91,13 +92,15 @@ class Enemy():
 score = 0
 
 def draw_score():
-    surf = FONT.render(str(score), False, BLACK)
+    surf = FONT.render(str(score), False, BLACK if current_color else WHITE)
     scr.blit(surf, (WIDTH - surf.get_width(), 0))
 
 gierek = Player(MATEUSZ)
 enemies = [Enemy(ENEMY) for _ in range(1)]
 
 game_time = 1
+friction = 0.97
+current_color = 1
 
 running = 1
 while running:
@@ -106,19 +109,25 @@ while running:
             running = 0
     
     k_pressed = pygame.key.get_pressed()
-    
-    gierek.x_vel *= 0.96 # tarcie
 
     if k_pressed[pygame.K_d]:
         gierek.x_vel += gierek.accel
     if k_pressed[pygame.K_a]:
         gierek.x_vel -= gierek.accel
+    if k_pressed[pygame.K_s]:
+        friction = 0.8
+    else:
+        friction = 0.97
+
+    gierek.x_vel *= friction # tarcie
     gierek.rect.x += gierek.x_vel
     
-    print(game_time)
-    if game_time % 100 == 0:
-        enemies.append(Enemy(ENEMY))
+    if game_time % 400 == 0:
+        for _ in range(2): enemies.append(Enemy(ENEMY))
     
+    if game_time % 500 == 0:
+        current_color = not current_color
+
     if gierek.rect.left < 0:
         nowy_gierk = Player(MATEUSZ)
         nowy_gierk.rect.x = gierek.rect.x + WIDTH
@@ -135,6 +144,11 @@ while running:
     elif gierek.rect.right > WIDTH + gierek.rect.w:
         gierek.rect.x -= WIDTH
 
+    if nowy_gierk is not None:
+        nowy_gierk.draw()
+
+    gierek.draw()
+
     for enemy in enemies:
         enemy.update()
         enemy.draw()
@@ -142,15 +156,11 @@ while running:
             gierek.death(title="WYNIK: " + str(score))
             running = 0
     
-    if nowy_gierk is not None:
-        nowy_gierk.draw()
-
-    gierek.draw()
 
     draw_score()
 
-    pygame.display.flip()
-    scr.fill(WHITE)
+    pygame.display.update()
+    scr.fill(WHITE if current_color else BLACK)
     clock.tick(75)
     game_time += 1
     score = game_time // 50 * 50
